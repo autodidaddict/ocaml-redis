6cdfe5ab91c7eb615abf0f66b2f4e62d392b388a
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2025-04-15 11:58:44 -0400

    foo

--- src/server.ml
+++ src/server.ml
@@ -6,15 +6,20 @@ let traceln fmt = traceln ("server: " ^^ fmt)
 module Read = Eio.Buf_read
 
 (* Read one line from [client] and respond with "OK". *)
-let rec handle_client flow addr =
-  traceln "Accepted connection from %a" Eio.Net.Sockaddr.pp addr;
+let handle_client flow addr =
+  traceln "Reading line from %a" Eio.Net.Sockaddr.pp addr;
   (* We use a buffered reader because we may need to combine multiple reads
      to get a single line (or we may get multiple lines in a single read,
      although here we only use the first one). *)
-  let from_client = Read.of_flow flow ~max_size:100 in
+  let from_client = Read.of_flow flow ~max_size:1024 in
   traceln "Received: %S" (Read.line from_client);
   Eio.Flow.copy_string "+PONG\r\n" flow;
+  Eio.Flow.copy_string "+PONG\r\n" flow;
+  traceln "sent"
+
+(*
   handle_client flow addr
+*)
 
 (* Accept incoming client connections on [socket].
    We can handle multiple clients at the same time.
