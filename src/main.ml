562e054c06c2945fff1c4a4a59a79c71180283bd
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2025-04-15 11:16:17 -0400

    urgh

--- src/main.ml
+++ src/main.ml
@@ -2,10 +2,15 @@ open Eio.Std
 
 let addr = `Tcp (Eio.Net.Ipaddr.V4.loopback, 6379)
 
+(* Run a server and a test client, communicating using [net]. *)
 let main ~net =
   Switch.run ~name:"main" @@ fun sw ->
+  (* We create the listening socket first so that we can be sure it is ready
+     as soon as the client wants to use it. *)
   let listening_socket = Eio.Net.listen net ~sw ~reuse_addr:true ~backlog:5 addr in
-(*   Fiber.fork_daemon ~sw (fun () -> Server.run listening_socket) *)
+  (* Start the server running in a new fiber.
+     Using [fork_daemon] here means that it will be stopped once the client is done
+     (we don't wait for it to finish because it will keep accepting new connections forever). *)
   Server.run listening_socket
 
 let () =
